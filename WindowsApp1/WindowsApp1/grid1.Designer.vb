843542a598b6dbb63d0bf227b4b51d308805915b
LAPTOP-JAD3H9SM\syuichi <syuichikumagai@gmail.com>
2021-04-18 10:14:10 +0900

    Revert "20210417　２回目テスト色変更"
    This reverts commit 8d0118d706dd553a877b61b399f3af9b38d9de74.

--- WindowsApp1/WindowsApp1/grid1.Designer.vb
+++ WindowsApp1/WindowsApp1/grid1.Designer.vb
@@ -22,7 +22,6 @@ Partial Class grid1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(grid1))
         Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
@@ -41,78 +40,95 @@ Partial Class grid1
         '
         'Label1
         '
-        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 384)
         Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
         '
         'Button1
         '
-        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Location = New System.Drawing.Point(25, 22)
         Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(135, 44)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.col_a, Me.col_b, Me.col_c, Me.col_d})
-        resources.ApplyResources(Me.DataGridView1, "DataGridView1")
+        Me.DataGridView1.Location = New System.Drawing.Point(29, 98)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowTemplate.Height = 21
+        Me.DataGridView1.Size = New System.Drawing.Size(743, 256)
+        Me.DataGridView1.TabIndex = 2
         '
         'Button2
         '
-        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Location = New System.Drawing.Point(198, 22)
         Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(135, 44)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Label2
         '
-        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 74)
         Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Label2"
         '
         'col_a
         '
-        DataGridViewCellStyle1.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
         DataGridViewCellStyle1.NullValue = Nothing
         Me.col_a.DefaultCellStyle = DataGridViewCellStyle1
-        resources.ApplyResources(Me.col_a, "col_a")
+        Me.col_a.HeaderText = "col_a"
         Me.col_a.Name = "col_a"
         '
         'col_b
         '
-        DataGridViewCellStyle2.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.col_b.DefaultCellStyle = DataGridViewCellStyle2
-        resources.ApplyResources(Me.col_b, "col_b")
+        Me.col_b.HeaderText = "col_b"
         Me.col_b.Name = "col_b"
         '
         'col_c
         '
-        DataGridViewCellStyle3.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
         DataGridViewCellStyle3.Format = "C2"
         DataGridViewCellStyle3.NullValue = Nothing
         Me.col_c.DefaultCellStyle = DataGridViewCellStyle3
-        resources.ApplyResources(Me.col_c, "col_c")
+        Me.col_c.HeaderText = "col_c"
         Me.col_c.Name = "col_c"
         '
         'col_d
         '
-        DataGridViewCellStyle4.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
         DataGridViewCellStyle4.Format = "N2"
         DataGridViewCellStyle4.NullValue = Nothing
         Me.col_d.DefaultCellStyle = DataGridViewCellStyle4
-        resources.ApplyResources(Me.col_d, "col_d")
+        Me.col_d.HeaderText = "col_d"
         Me.col_d.Name = "col_d"
         '
         'grid1
         '
-        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.CausesValidation = False
+        Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "grid1"
+        Me.Text = "grid1"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
